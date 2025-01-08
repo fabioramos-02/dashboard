c72c57681f3ba54d6e73d447dc774741685155df
Fabio Ramos <fabiowilliam466@gmail.com>
2025-01-08 15:07:08 -0400

    Adiciona suporte para exibição de imagens do GitHub no dashboard e configura domínio permitido para imagens

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,35 @@
 import { getServerSession } from "next-auth";
 import ButtonLogout from "./ButtonLogout";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+
 export default async function Page() {
   const session = await getServerSession();
+
+  // Ver os dados da sessão
+  console.log(session);
+
+  // Redirecionar para a página inicial se o usuário não estiver autenticado
   if (!session) {
-    redirect("/")
+    redirect("/");
   }
 
   return (
     <div>
       <div>Olá {session?.user?.name}</div>
       <div>Dashboard</div>
+
+      {/* Mostrar imagem do GitHub com 300px de largura */}
+      <div>
+        <Image
+          src={session?.user?.image || ""} // Coloque uma imagem padrão se o usuário não tiver foto
+          alt={session?.user?.name || "Usuário"}
+          width={300} // Define a largura
+          height={300} // Define a altura
+          style={{ borderRadius: "5%" }} // Opcional: Para deixar a imagem redonda
+        />
+      </div>
+
       <div>
         <ButtonLogout />
       </div>
