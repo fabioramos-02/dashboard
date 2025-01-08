02cbae79b40a1478571fb49bcfeeddbe9c259bc6
Fabio Ramos <fabiowilliam466@gmail.com>
2025-01-08 14:56:59 -0400

    Adiciona funcionalidade de logout e redirecionamento para usuários não autenticados no dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
 import { getServerSession } from "next-auth";
-
+import ButtonLogout from "./ButtonLogout";
+import { redirect } from "next/navigation";
 export default async function Page() {
   const session = await getServerSession();
+  if (!session) {
+    redirect("/")
+  }
+
   return (
     <div>
       <div>Olá {session?.user?.name}</div>
       <div>Dashboard</div>
+      <div>
+        <ButtonLogout />
+      </div>
     </div>
   );
 }
